ddba0e853e2d84fea9d3d54024eb2ecf4ad7b6fa
alexanderluukkonens <alexanderluukkonens@hotmail.com>
2025-03-19 14:12:04 +0100

    Feat:
    - Add Gamedescription component for GamePage
    
    style:
    
    - Add Gamedescription.css in GamePage folder

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router";
 import useGameStore from "../state/useGameStore";
 import { useEffect } from "react";
+import { GameDescription } from "../components/GamePage/GameDescription";
 
 export function GamePage() {
   const { id } = useParams();
@@ -15,24 +16,23 @@ export function GamePage() {
     <>
       <div className="GamePage">
         <main className="game-container">
-        <div>GameTitle</div>
-        <div>GameMedia</div>
-        <div className="right-side-content">
-          <div>Header/banner image</div>
-          <div>Details List</div>
-          <div>Tags</div>
-          <div>ActionButtons</div>
-        </div>
-        <div className="game-details">
-          <div>Short Description</div>
-          <div>More About this Game (Detailed Description)</div>
-          <div>System Requirements</div>
-        </div>
+          <div>GameTitle</div>
 
-        <p>{selectedGame.id}</p>
+          <div>GameMedia</div>
+          <div className="right-side-content">
+            <div>Header/banner image</div>
+            <div>Details List</div>
+            <div>Tags</div>
+            <div>ActionButtons</div>
+          </div>
+          <div className="game-details">
+            <GameDescription />
+            <div>More About this Game (Detailed Description)</div>
+            <div>System Requirements</div>
+          </div>
 
+          <p>{selectedGame.id}</p>
         </main>
-        
       </div>
     </>
   );
