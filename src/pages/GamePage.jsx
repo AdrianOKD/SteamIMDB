10dd2d81dc6a705b218e8a88677ad952dc84632e
LudwigStenberg <contact@ludwigtenberg.com>
2025-03-20 15:14:13 +0100

    Build Tags and TagsModal

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -3,10 +3,11 @@ import useGameStore from "../state/useGameStore";
 import { useEffect } from "react";
 import { GameDetailsList } from "../components/GamePage/GameDetailsList";
 import { GameDescription } from "../components/GamePage/GameDescription";
+import DetailedDescription from "../components/GamePage/DetailedDescription";
+import Tags from "../components/GamePage/Tags";
 import "/src/Css/gamepage/GameTitle.css";
 import "/src/Css/gamepage/GameContainer.css";
 import "./GamePage.css";
-import DetailedDescription from "../components/GamePage/DetailedDescription";
 
 export function GamePage() {
   const { id } = useParams();
@@ -14,6 +15,7 @@ export function GamePage() {
   const selectGame = useGameStore((state) => state.selectGame);
   useEffect(() => {
     selectGame(games, id);
+    console.log("Selected Game:", selectedGame);
   }, [id, games, selectGame]);
   const selectedGame = useGameStore((state) => state.selectedGame);
   return (
@@ -24,8 +26,10 @@ export function GamePage() {
           <div>GameMedia</div>
           <div className="right-side-content">
             <div>Header/banner image</div>
-            <div><GameDetailsList selectedGame={selectedGame} /></div>
-            <div>Tags</div>
+            <div>
+              <GameDetailsList selectedGame={selectedGame} />
+            </div>
+            <Tags />
             <div>ActionButtons</div>
           </div>
           <div className="game-details">
