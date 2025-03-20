05ed15a400218b805d091aaf56df3798a5e48b46
Andreas Fagerberg <andreas_fagerberg@outlook.com>
2025-03-20 14:19:24 +0100

    feat: add functionality to render screenshots on the game page. - Screenshots from selected game now renders properly.
    add global state for tracking screenshots.
    - useGameStore now tracks the selected game's
    screenshot

--- src/components/GamePage/GameScreenshot.jsx
+++ src/components/GamePage/GameScreenshot.jsx
@@ -1,14 +1,15 @@
-import "./GameScreenshot.css"
+import "./GameScreenshot.css";
 import { Grid2 } from "@mui/material";
 
 export function GameScreenshot({ variant = "medium", ...props }) {
-  const isValidVariant = variant === "small" || variant === "medium" || variant === "large";
+  const isValidVariant =
+    variant === "small" || variant === "medium" || variant === "large";
   const actualVariant = isValidVariant ? variant : "medium";
-  
+
   const gridSize = {
     small: 2,
     medium: 6,
-    large: 12
+    large: 12,
   }[actualVariant];
 
   return (
